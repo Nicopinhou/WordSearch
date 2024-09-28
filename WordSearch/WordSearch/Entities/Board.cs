79ae4ae3d1c72f4b3391ab3ee5f1b5754fb4b2ea
nicopinhou <almeidanicolassantana@gmail.com>
2024-09-28 00:50:05 -0300

    Board class application

---
+++ WordSearch/WordSearch/Entities/Board.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSearch.Entities
+{
+    internal class Board
+    {
+        public char[,] Characters { get; set; }
+
+        public Board() { 
+            Characters = new char[0,0];
+        }
+
+        public Board(char[,] characters)
+        {
+            Characters = characters.Clone() as char[,];
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            foreach (char c in Characters)
+            {
+                if (i % Characters.GetLength(1) == 0) sb.Append("\n" + c); else sb.Append(" " + c);
+                i++;
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ WordSearch/WordSearch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+using WordSearch.Entities;
+
+namespace WordSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Board a = new Board()
+            {
+                Characters = new char[,] { { 'a', 'a', 'b' },
+                                          { 'b', 'a', 't' }  }
+            };
+            Console.WriteLine(a);
+        }
+    }
+}
